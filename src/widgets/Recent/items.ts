dc25f1d71afccd8b723355973feb12a564650b26
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-05-16 00:11:58 -0700

    font-size

--- src/widgets/Recent/items.ts
+++ src/widgets/Recent/items.ts
@@ -1,7 +1,7 @@
 export const items = [
   {
     id: 1,
-    text: 'This React application is a set of functionalities implemented using an architectural methodology - Feature-Sliced Design (FSD)',
+    text: 'This React application is a set of functionalities implemented using an architectural methodology - Feature-Sliced Design (FSD).',
     url: 'https://react-application-ci-cd.vercel.app/',
   },
   {
@@ -11,52 +11,52 @@ export const items = [
   },
   {
     id: 3,
-    text: 'This application combines an online store and b2b. Available if you are in Russia or when using a VPN',
+    text: 'This application combines an online store and b2b. Available if you are in Russia or when using a VPN.',
     url: 'https://www.etm.ru/ipro3',
   },
   {
     id: 4,
-    text: 'Website for a custom cake making company',
+    text: 'Website for a custom cake making company.',
     url: 'https://palettedessert.ru/',
   },
   {
     id: 5,
-    text: 'Website for a private school that teaches Italian',
+    text: 'Website for a private school that teaches Italian.',
     url: 'https://corsiditaliano.ru/',
   },
   {
     id: 6,
-    text: 'Website for a service for ordering the services of professional nurses',
+    text: 'Website for a service for ordering the services of professional nurses.',
     url: 'https://gracerussia.com/',
   },
   {
     id: 7,
-    text: 'Website for a company engaged in book production',
+    text: 'Website for a company engaged in book production.',
     url: 'https://xn--80aaxllhddj8i.xn--p1ai/',
   },
   {
     id: 8,
-    text: 'Website for a food delivery company Papa Panda',
+    text: 'Website for a food delivery company Papa Panda.',
     url: 'https://fr.expresspanda.ru/',
   },
   {
     id: 9,
-    text: 'Website for a company promoting new brands',
+    text: 'Website for a company promoting new brands.',
     url: 'https://breakingtrends.io/',
   },
   {
     id: 10,
-    text: 'Website for a pharmacy chain franchise',
+    text: 'Website for a pharmacy chain franchise.',
     url: 'https://franshiza-apteki.ru/',
   },
   {
     id: 11,
-    text: 'Website for a private motorcycle school',
+    text: 'Website for a private motorcycle school.',
     url: 'https://alexandr-andreev.ru/',
   },
   {
     id: 12,
-    text: 'Website for a company engaged in a full cycle of Internet marketing',
+    text: 'Website for a company engaged in a full cycle of Internet marketing.',
     url: 'https://lionadverts.com/',
   },
 ]
